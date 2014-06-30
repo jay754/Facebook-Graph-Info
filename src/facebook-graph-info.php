14d2847a21e9050c978ab51c6f1ddafa7e82f9b9
Jay Engineer <jayengineer6@gmail.com>
2014-06-30 19:25:31 -0400

    w

--- src/facebook-graph-info.php
+++ src/facebook-graph-info.php
@@ -10,9 +10,7 @@
 	license: The BSD 3-Clause License
 */
 
-class FacebookGraph
-
-{
+class FacebookGraph {
 	// All the info you can get from facebook without oAuth
 	
 	private $_url; //facebook api url
@@ -20,8 +18,8 @@ class FacebookGraph
 	public $http_status; // http status 
 	
 	public function __construct($url, $token = NULL){
-		$this -> _url = $url; //url
-		$this -> _token = $token; //access token
+        $this -> _url = $url; //url
+        $this -> _token = $token; //access token
 	}
 	
 	/**
@@ -34,8 +32,8 @@ class FacebookGraph
 	**/
 
 	public static function HTTPstatus($url) {
-		$headers = get_headers($url);
-		return substr($headers[0], 9, 3); //returns http status
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3); //returns http status
 	}
 	
 	/**
@@ -47,9 +45,9 @@ class FacebookGraph
 	**/
 	
 	public static function getURL(){
-		$url = $this -> _url;
+        $url = $this -> _url;
 		
-		return $url;
+        return $url;
 	}
 	
 	/** 
@@ -60,11 +58,11 @@ class FacebookGraph
 	**/
 	
 	public function fbInfo($id){
-		$url = $this-> _url;
-		$info = file_get_contents($url.'/'.$id);
-		$data = json_decode($info, true);
+        $url = $this-> _url;
+        $info = file_get_contents($url.'/'.$id);
+        $data = json_decode($info, true);
 
-		return $data;
+        return $data;
 	}
 	
 	/** 
@@ -75,10 +73,10 @@ class FacebookGraph
 	**/
 
 	public function fbID($fb_username){
-		$info = $this -> fbInfo($fb_username);
-		$id = $info['id'];
+        $info = $this -> fbInfo($fb_username);
+        $id = $info['id'];
 
-		return $id;
+        return $id;
 	}
 
 	/** 
@@ -91,10 +89,9 @@ class FacebookGraph
 	public function getName($fb_username){
 		$info = $this -> fbInfo($fb_username);
 		$data = array("first_name" => $info['first_name'],
-					  "last_name" => $info['last_name'],
-					  );
-		
-		return $data;
+                      "last_name" => $info['last_name']);
+        
+        return $data;
 	}
 	
 	/** 
@@ -105,10 +102,10 @@ class FacebookGraph
 	**/
 	
 	public function fbUsername($username){
-		$info = $this -> fbInfo($username);
-		$username = $info["username"];
-		
-		return $username;
+        $info = $this -> fbInfo($username);
+        $username = $info["username"];
+
+        return $username;
 	}
 	
 	/** 
@@ -119,11 +116,11 @@ class FacebookGraph
 	**/
 	
 	public function getLink($username){
-		$info = $this-> fbInfo($username);
-		$link = $info["link"];
-		
-		return $link;
-	}
+        $info = $this-> fbInfo($username);
+        $link = $info["link"];
+
+        return $link;
+    }
 	
 	/** 
 		getGender Method
@@ -133,10 +130,10 @@ class FacebookGraph
 	**/
 	
 	public function getGender($username){
-		$info = $this-> fbInfo($username);
-		$gender = $info["gender"];
+        $info = $this-> fbInfo($username);
+        $gender = $info["gender"];
 
-		return $gender;
+        return $gender;
 	}
 	
 //basic info out of the way	
@@ -150,11 +147,11 @@ class FacebookGraph
 	**/
 	
 	public function getPic($id){
-		$url = $this-> _url;
-		header('Content-Type: image/x-png');
-		$photo = file_get_contents('http://graph.facebook.com/'.$id.'/picture');
+        $url = $this-> _url;
+        header('Content-Type: image/x-png');
+        $photo = file_get_contents('http://graph.facebook.com/'.$id.'/picture');
 
-		return $photo;
+        return $photo;
 	}
 	
 	/** 
@@ -165,29 +162,30 @@ class FacebookGraph
 	**/
 
 	public function getPageInfo($Id){
-		$url = $this-> _url;
-		$info = file_get_contents($url.'/'.$Id);
-		$json = json_decode($info, true);
+        $url = $this-> _url;
+        $info = file_get_contents($url.'/'.$Id);
+        $json = json_decode($info, true);
 		
-		try
-		{
-			if (empty($json["location"])){
-				throw new Exception('City is not Listed.');
+        try
+        {
+            if (empty($json["location"])){
+                throw new Exception('City is not Listed.');
 			}
 		}
-		catch (Exception $e)
-		{
-		  print $e->getMessage();
-		}
+        catch (Exception $e)
+        {
+            print $e->getMessage();
 		
-		$data = array("Name" => $json["name"],
-					  "Id" => $json["id"],
-					  "likes" => $json["likes"],
-					  "website" => $json["website"],
-					  "People Talking about" => $json["talking_about_count"],
-					  "City" => $json["location"]["city"]);
+        $data = array("Name" => $json["name"],
+                      "Id" => $json["id"],
+                      "likes" => $json["likes"],
+                      "website" => $json["website"],
+                      "People Talking about" => $json["talking_about_count"],
+                      "City" => $json["location"]["city"]);
 
-		return $data;
+        return $data;
+
+		}
 	}
 	
 	/** 
@@ -202,12 +200,12 @@ class FacebookGraph
 		$info = file_get_contents($url.'/'.$appId);
 		$json = json_decode($info, true);
 		$data = array("name" => $json["name"],
-					  "weekly active users" => $json["weekly_active_users"],
-					  "monthly active users" => $json["monthly_active_users"],
-					  "daily active users_rank" => $json["daily_active_users_rank"],
-					  "monthly active users_rank" => $json["monthly_active_users_rank"]);
-		
-		return $data;
+                      "weekly active users" => $json["weekly_active_users"],
+                      "monthly active users" => $json["monthly_active_users"],
+                      "daily active users_rank" => $json["daily_active_users_rank"],
+                      "monthly active users_rank" => $json["monthly_active_users_rank"]);
+
+        return $data;
 	}
 	
 	/** 
@@ -219,29 +217,29 @@ class FacebookGraph
 	**/
 	
 	public function getLikes($id){
-	
-		$url = $this-> _url;
-		$token = $this-> _token;
-		$http_status = $this-> HTTPstatus($url."/".$id."/likes?access_token=".$token); //http status 
+
+        $url = $this-> _url;
+        $token = $this-> _token;
+        $http_status = $this-> HTTPstatus($url."/".$id."/likes?access_token=".$token); //http status 
 		
-		if ($http_status == 200){
-			$info = file_get_contents($url."/".$id."/likes?access_token=".$token);
-			$json = json_decode($info, true);
-			$data = array();
-			
-			for($i=0;$i<sizeof($json['data']);$i++){
-				$data[$i]['category'] = $json['data'][$i]['category'];
-				$data[$i]['name'] = $json['data'][$i]['name'];
-				$data[$i]['id'] = $json['data'][$i]['id'];
-				$data[$i]['created_time'] = $json['data'][$i]['created_time'];
-			}
-			
-			return $data;
-		}
+        if ($http_status == 200){
+            $info = file_get_contents($url."/".$id."/likes?access_token=".$token);
+            $json = json_decode($info, true);
+            $data = array();
+
+            for($i=0;$i<sizeof($json['data']);$i++){
+                $data[$i]['category'] = $json['data'][$i]['category'];
+                $data[$i]['name'] = $json['data'][$i]['name'];
+                $data[$i]['id'] = $json['data'][$i]['id'];
+                $data[$i]['created_time'] = $json['data'][$i]['created_time'];
+            }
+
+            return $data;
+        }
 		
 		else {
-			return "bad request";
-		}
+            return "bad request";
+        }
 	}
 	
 	/** 
@@ -253,27 +251,26 @@ class FacebookGraph
 	**/
 	
 	public function getFriends($id){
-		$url = $this-> _url;
-		$token = $this-> _token;
-		$http_status = $this-> HTTPstatus($url."/".$id."/friends?access_token=".$token); //http status 
-		
-		if ($http_status == 200){
-			$info = file_get_contents($url."/".$id."/friends?access_token=".$token);
-			$json = json_decode($info, true);
-			$data = array();
-			
-			for($i=0;$i<sizeof($json['data']);$i++){
-				$data[$i]['name'] = $json['data'][$i]['name'];
-				$data[$i]['id'] = $json['data'][$i]['id'];
-			}
-			
-			return $data;
-		}
-		
-		else {
-			return "bad request";
-		}
-	}
+        $url = $this-> _url;
+        $token = $this-> _token;
+        $http_status = $this-> HTTPstatus($url."/".$id."/friends?access_token=".$token); //http status 
+
+        if ($http_status == 200){
+            $info = file_get_contents($url."/".$id."/friends?access_token=".$token);
+            $json = json_decode($info, true);
+            $data = array();
+
+            for($i=0;$i<sizeof($json['data']);$i++){
+                $data[$i]['name'] = $json['data'][$i]['name'];
+                $data[$i]['id'] = $json['data'][$i]['id'];
+            }
+
+            return $data;
+        }
+        else {
+            return "bad request";
+        }
+    }
 	
 	/** 
 		getGroups Method
@@ -284,55 +281,53 @@ class FacebookGraph
 	**/
 	
 	public function getGroups($id){
-		
-		$url = $this-> _url;
-		$token = $this-> _token;
-		$http_status = $this-> HTTPstatus($url."/".$id."/groups?access_token=".$token); //http status 
-		
-		if ($http_status == 200){
-			$info = file_get_contents($url."/".$id."/groups?access_token=".$token);
-			$json = json_decode($info, true);
-			$data = array();
+        $url = $this-> _url;
+        $token = $this-> _token;
+        $http_status = $this-> HTTPstatus($url."/".$id."/groups?access_token=".$token); //http status 
+
+        if ($http_status == 200){
+            $info = file_get_contents($url."/".$id."/groups?access_token=".$token);
+            $json = json_decode($info, true);
+            $data = array();
+
+            for($i=0;$i<sizeof($json['data']);$i++){
+                $data[$i]['name'] = $json['data'][$i]['name'];
+                $data[$i]['id'] = $json['data'][$i]['id'];
+                $data[$i]['version'] = $json['data'][$i]['version'];
+                $data[$i]['unread_messages'] = $json['data'][$i]['unread'];
+                $data[$i]['admin'] = $json['data'][$i]['administrator'];
+            }
 			
-			for($i=0;$i<sizeof($json['data']);$i++){
-				$data[$i]['name'] = $json['data'][$i]['name'];
-				$data[$i]['id'] = $json['data'][$i]['id'];
-				$data[$i]['version'] = $json['data'][$i]['version'];
-				$data[$i]['unread_messages'] = $json['data'][$i]['unread'];
-				$data[$i]['admin'] = $json['data'][$i]['administrator'];
-			}
-			
-			return $data;
-		}
-		else {
-			return "bad request";
-		}
-	}
+            return $data;
+        }
+        else {
+            return "bad request";
+        }
+    }
 	
 	public function getMusic($id){
-		$url = $this->_url;
-		$token = $this->_token;
-		$http_status = $this-> HTTPstatus($url."/".$id."/music?access_token=".$token); //http status 
-		
-		if ($http_status == 200){
-			$info = file_get_contents($url."/".$id."/music?access_token=".$token);
-			$json = json_decode($info, true);
-			$data = array();
-			
-			for($i=0;$i<sizeof($json['data']);$i++){
-				$data[$i]['name'] = $json['data'][$i]['name'];
-				$data[$i]['id'] = $json['data'][$i]['id'];
-				$data[$i]['category'] = $json['data'][$i]['category'];
-				$data[$i]['created_time'] = $json['data'][$i]['created_time'];
-			}
-			
-			return $data; //get results
-		}
-		
-		else {
-			return "bad request";
-		}
-	}
+        $url = $this->_url;
+        $token = $this->_token;
+        $http_status = $this-> HTTPstatus($url."/".$id."/music?access_token=".$token); //http status 
+
+        if ($http_status == 200){
+            $info = file_get_contents($url."/".$id."/music?access_token=".$token);
+            $json = json_decode($info, true);
+            $data = array();
+
+            for($i=0;$i<sizeof($json['data']);$i++){
+                $data[$i]['name'] = $json['data'][$i]['name'];
+                $data[$i]['id'] = $json['data'][$i]['id'];
+                $data[$i]['category'] = $json['data'][$i]['category'];
+                $data[$i]['created_time'] = $json['data'][$i]['created_time'];
+            }
+
+            return $data; //get results
+        }
+        else {
+            return "bad request";
+        }
+    }
 } //classend
 
 $facebookObj = new FacebookGraph('https://graph.facebook.com','AAAAAAITEghMBANUwJj2k1T5X48lGNZAm2vEb9aEW6CYsWV2koEZAruE8i2uBhTQ2JjZA1vdYbxUgve6uRKIUac21jgXhZBxDBNEasDtOVlEYBG3tNi5V'); //The URL
